ce8b1d9c4ee12899945fc5a1720bf48a61c8d7d9
AD <70889070+Adstillcodes@users.noreply.github.com>
2022-07-31 20:15:49 +0530

    Add files via upload

---
+++ QRCODECREATOR/main.py
@@ -0,0 +1,8 @@
+import pyqrcode
+from pyzbar.pyzbar import decode
+from PIL import Image 
+qr1 = input("link you want the QRcode to display eg:www.google.com : ")
+qr2 = input("name of the image...please add a .jpg at the end of the name thnx!!: ")
+qr = pyqrcode.create(qr1)
+qr.png(qr2, scale=8)
+
