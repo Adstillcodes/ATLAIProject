a036fdb45ec4c4a530cb6536520bfafdf8d3a5f9
AD <70889070+Adstillcodes@users.noreply.github.com>
2022-08-13 22:34:11 +0530

    Update Faceregv2.py

--- Faceregv2.py
+++ Faceregv2.py
@@ -1,90 +1,108 @@
-#V1.01 of code for ATL project
-
+#V2.0 of ATLAI Proj
 import face_recognition
+import os, sys
 import cv2
 import numpy as np
-import csv
-import os
+import math
 
-from datetime import datetime
-#opencv video capture settings
-video_capture = cv2.VideoCapture(2)
-#defining images
-jobs_image = face_recognition.load_image_file("photos/SteveJobs.jpg")
-jobs_encoding = face_recognition.face_encodings(jobs_image)[0]
 
-ratan_tata_image = face_recognition.load_image_file("photos/RatanTata.jpg")
-ratan_tata_encoding = face_recognition.face_encodings(ratan_tata_image)[0]
+# Helper
+def face_confidence(face_distance, face_match_threshold=0.6):
+    range = (1.0 - face_match_threshold)
+    linear_val = (1.0 - face_distance) / (range * 2.0)
 
-ad_image = face_recognition.load_image_file("photos/ad_image.jpg")
-ad_encoding = face_recognition.face_encodings(ad_image)[0]
+    if face_distance > face_match_threshold:
+        return str(round(linear_val * 100, 2)) + '%'
+    else:
+        value = (linear_val + ((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2, 0.2))) * 100
+        return str(round(value, 2)) + '%'
 
-known_face_encoding = [
-jobs_encoding,
-ratan_tata_encoding,
-ad_encoding
-]
 
-known_face_names = [
-    "Steve Jobs",
-    "Ratan Tata",
-    "Anvit Deshpande"
-]
+class FaceRecognition:
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    known_face_encodings = []
+    known_face_names = []
+    process_current_frame = True
 
-students = known_face_names.copy()
+    def __init__(self):
+        self.encode_faces()
 
-face_locations = []
-face_encodings = []
-face_names = []
-s=True
+    def encode_faces(self):
+        for image in os.listdir('faces'):
+            face_image = face_recognition.load_image_file(f"faces/{image}")
+            face_encoding = face_recognition.face_encodings(face_image)[0]
 
-#creating the attendance file
-now = datetime.now()
-current_date = now.strftime("%Y-%m-%d")
+            self.known_face_encodings.append(face_encoding)
+            self.known_face_names.append(image)
+        print(self.known_face_names)
 
-f = open(current_date+' .csv', 'w+', newline = '')
-lnwriter = csv.writer(f)
+    def run_recognition(self):
+        video_capture = cv2.VideoCapture(1)
 
-#opencv video capture 
+        if not video_capture.isOpened():
+            sys.exit('Video source not found...')
 
-while True: 
-    _,frame = video_capture.read()
-    small_frame = cv2.resize(frame, (0,0),fx=0.25,fy=0.25) #decreasing the size of the frame
-    rgb_small_frame = small_frame[:,:,::-1]
-    if s:
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame,face_locations)
-        face_names = []
-        for face_encoding in face_encodings:
-            matches = face_recognition.compare_faces(known_face_encoding, face_encoding)
-            name = ""
-            face_distance = face_recognition.face_distance(known_face_encoding,face_encoding)
-            best_match_index = np.argmin(face_distance)
-            if matches[best_match_index]:
-                name = known_face_names[best_match_index]
-            for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        while True:
+            ret, frame = video_capture.read()
+
+            # Only process every other frame of video to save time
+            if self.process_current_frame:
+                # Resize frame of video to 1/4 size for faster face recognition processing
+                small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+                # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+                rgb_small_frame = small_frame[:, :, ::-1]
+
+                # Find all the faces and face encodings in the current frame of video
+                self.face_locations = face_recognition.face_locations(rgb_small_frame)
+                self.face_encodings = face_recognition.face_encodings(rgb_small_frame, self.face_locations)
+
+                self.face_names = []
+                for face_encoding in self.face_encodings:
+                    # See if the face is a match for the known face(s)
+                    matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+                    name = "Unknown"
+                    confidence = '???'
+
+                    # Calculate the shortest distance to face
+                    face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+
+                    best_match_index = np.argmin(face_distances)
+                    if matches[best_match_index]:
+                        name = self.known_face_names[best_match_index]
+                        confidence = face_confidence(face_distances[best_match_index])
+
+                    self.face_names.append(f'{name} ({confidence})')
+
+            self.process_current_frame = not self.process_current_frame
+
+            # Display the results
+            for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
+                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
+
+                # Create the frame with the name
                 cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
+
+            # Display the resulting image
+            cv2.imshow('Face Recognition', frame)
+
+            # Hit 'q' on the keyboard to quit!
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+        # Release handle to the webcam
+        video_capture.release()
+        cv2.destroyAllWindows()
 
 
-            #entering names in the CSV file
-            face_names.append(name)
-            if name in known_face_names:
-                if name in students:
-                    students.remove(name)
-                    print(students)
-                    current_time = now.strftime("%H-%M-%S")
-                    lnwriter.writerow([name,current_time])
-
-    #showing the output to the user
-    cv2.imshow("attendance system", frame)
-    #pressing "Q" to "quit"
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-video_capture.release()
-cv2.destroyAllWindows()
-f.close()
-    
+if __name__ == '__main__':
+    fr = FaceRecognition()
+    fr.run_recognition()
